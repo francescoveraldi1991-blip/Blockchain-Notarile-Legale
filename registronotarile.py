624a5434b363495d9770e46e9fc37fa5a7ed19eb
francescoveraldi1991-blip <francesco.veraldi1991@gmail.com>
2026-02-22 22:08:12 +0100

    registronotarile.py

--- registronotarile.py
+++ registronotarile.py
@@ -49,8 +49,7 @@ with col1:
     
     # --- IL PULSANTE DI COLLEGAMENTO ---
     if st.button("ENTRA NELLA BLOCKCHAIN LEGALE"):
-        st.switch_page("pages/1_Contratti_Blockchain.py")
-
+    st.switch_page("pages/1_Notarizzazione.py")
 with col2:
     st.markdown("![Legal Icon](https://img.icons8.com/ios-filled/150/1a2a6c/law.png)")
 
